452d425f873603ee08efe47f952dcf30c2d45006
tk382 <tk382@uchicago.edu>
2020-03-31 12:15:32 -0500

    data set documentation

---
+++ R/data.R
@@ -0,0 +1,16 @@
+#' A sample single cell sequencing data subsetted from Zheng2017
+#'
+#' @format Single Cell experiment object with 10,000 genes and 100 cells
+#' @source \url{https://www.nature.com/articles/ncomms14049}
+"toydata"
+
+
+#' A reference data frame that matches ENSG IDs to HGNC symbols
+#'
+#' @format A data frame with 46606 rows and 2 columns
+#' \describe{
+#'   \item{ensg}{Ensembl ENSG IDs}
+#'   \item{hgnc}{HGNC symbols}
+#' }
+#' @source \url{http://www.biomart.org/}
+"ensg_hgnc"
